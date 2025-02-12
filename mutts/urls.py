2cc169804c9970ad091b089ad73d86ce007fccb9
Danivalois <valois.daniela@gmail.com>
2025-02-12 19:57:43 -0300

    ain

--- mutts/urls.py
+++ mutts/urls.py
@@ -1,17 +1,20 @@
 from django.contrib import admin
-
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import home  # Import the new view
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('products/', include('products.urls')),
+    path("", home, name="home"),  # Add root path
+    path("admin/", admin.site.urls),
+    path("products/", include("products.urls")),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    
 
 
     
